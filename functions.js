24c73a0442762e9d974a2b129ec823c57bde392d
Isaiah Richardson <capcinematic@gmail.com>
2022-11-14 04:50:51 -0700

    Initial commit

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
-
+function printGreetings (Stark){
+    console.log(`Morning, Mr.${Stark}`)
+}
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
 
